ea24e6255a4b5a9bf0daa6173cc60cb60d695798
Jacob Issa <jacobisssa@gmail.com>
2022-05-09 23:08:47 +0200

    Update content-course.php

--- wp-content/themes/learning_aid/template-parts/content-course.php
+++ wp-content/themes/learning_aid/template-parts/content-course.php
@@ -6,7 +6,13 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 	<header class="entry-header has-text-align-center">
 		<div class="entry-header-inner section-inner medium">
-			<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+			<?php
+				if ( is_singular() ):
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else:
+					the_title( '<h2 class="entry-title heading-size-1"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+				endif;
+			?>
 			<div class="entry-categories">
 				<div class="entry-categories-inner">
 					<?php
@@ -25,5 +31,5 @@
 			<?php the_content(); ?>
 		</div>
 	</div>
-	<?php get_template_part('template-parts/navigation'); ?>
+	<?php if (is_single() ) { get_template_part('template-parts/navigation'); }?>
 </article>
