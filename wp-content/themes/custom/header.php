505cad5cb820b893a444e2ab121bc61895ea60f7
Manuel <waelzlein.manuel@web.de>
2022-06-03 12:13:31 +0200

    added home link to header

--- wp-content/themes/custom/header.php
+++ wp-content/themes/custom/header.php
@@ -45,6 +45,6 @@
             <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/>
         </svg>
     </button>
-    <h1 id="header_text"><b>Lernhilfe</b> - GrCMS Team 2</h1>
+    <a href="<?php echo home_url() ?>" id="header_anchor"><h1 id="header_text"><b>Lernhilfe</b> - GrCMS Team 2</h1></a>
 </header>
 <div id="content_container">
