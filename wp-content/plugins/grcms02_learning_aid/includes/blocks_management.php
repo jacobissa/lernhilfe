0870a7633ae44ebd17ed438b039cc7e6f890e376
Jacob Issa <jacobisssa@gmail.com>
2022-06-14 15:32:22 +0200

    remove core/shortcode and no need to import multiple-choice.php again

--- wp-content/plugins/grcms02_learning_aid/includes/blocks_management.php
+++ wp-content/plugins/grcms02_learning_aid/includes/blocks_management.php
@@ -6,18 +6,17 @@
  */
 require_once(LEARNINGAID_PLUGIN_LOCATION_DIR . '/blocks/ytvideo/block-ytvideo.php');
 require_once(LEARNINGAID_PLUGIN_LOCATION_DIR . '/blocks/textquestion/block-textquestion.php');
-require_once(WP_PLUGIN_DIR . '/multiple-choice/multiple-choice.php');
 
 
 /**
- * Allow only the gutenberg blocks ('ytvideo', 'testquestion', 'shortcode') in the post type 'lesson';
+ * Allow only the gutenberg blocks ('ytvideo', 'testquestion', 'multiple-choice') in the post type 'lesson';
  * all other blocks will be removed in this post type.
  * @Hook 'allowed_block_types'
  */
 function learningaid_allowed_block_types($allowed_block_types, $post)
 {
     if ($post->post_type === 'lesson') :
-        return array('learning-aid/block-textquestion', 'learning-aid/block-ytvideo', 'learning-aid/block-multiple-choice', 'core/shortcode');
+        return array('learning-aid/block-textquestion', 'learning-aid/block-ytvideo', 'learning-aid/block-multiple-choice');
     else :
         return $allowed_block_types;
     endif;
